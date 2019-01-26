ad7be6ed6146bd8a57bcae90432f911da72cbfa2
Christian Andersen <c.andersen2012@gmail.com>
2019-01-26 11:02:37 -0600

    Fixing ordering issue.

--- src/main/java/com/anderc/riptiderenamer/Main.java
+++ src/main/java/com/anderc/riptiderenamer/Main.java
@@ -1,30 +1,41 @@
 package com.anderc.riptiderenamer;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
 import com.anderc.riptiderenamer.delegate.AZLyricsDelegate;
+import com.anderc.riptiderenamer.delegate.GoogleAppsScriptDelegate;
+import com.anderc.riptiderenamer.delegate.GoogleCredentialDelegate;
 import com.anderc.riptiderenamer.delegate.GoogleSheetsDelegate;
 import com.anderc.riptiderenamer.model.Song;
 import com.anderc.riptiderenamer.service.RandomSleepService;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 
 public class Main {
 
-    public static void main(String... args) throws IOException, GeneralSecurityException, InterruptedException {
-        List<Song> songs =  GoogleSheetsDelegate.getSongs();
-        final AZLyricsDelegate lyricsDelegate = new AZLyricsDelegate();
-
-        for(int i=0; i<songs.size(); i++) {
-            RandomSleepService.sleep();
-            System.out.println(i + 1 + "/" + songs.size() + ": " + songs.get(i).getTitle() + " - " + songs.get(i).getArtist());
-            songs.get(i).setLyrics(lyricsDelegate.getLyrics(songs.get(i).getTitle(), songs.get(i).getArtist()));
-        }
+    public static void main(String... args) throws IOException, GeneralSecurityException, InterruptedException, URISyntaxException {
+//        List<Song> songs =  GoogleSheetsDelegate.getSongs();
+//        final AZLyricsDelegate lyricsDelegate = new AZLyricsDelegate();
+//
+//        for(int i=0; i<songs.size(); i++) {
+//            RandomSleepService.sleep();
+//            System.out.println(i + 1 + "/" + songs.size() + ": " + songs.get(i).getTitle() + " - " + songs.get(i).getArtist());
+//            songs.get(i).setLyrics(lyricsDelegate.getLyrics(songs.get(i).getTitle(), songs.get(i).getArtist()));
+//        }
 
 
 
         // set the lyrics into google sheets
-        GoogleSheetsDelegate.addLinksToSheet(songs);
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Credential credential = GoogleCredentialDelegate.getCredentials(HTTP_TRANSPORT);
+        GoogleAppsScriptDelegate appsScriptDelegate = new GoogleAppsScriptDelegate(credential, HTTP_TRANSPORT);
+
+        appsScriptDelegate.uploadScripts();
+
 
         // get the lyrics from
 //        String foo = new AZLyricsDelegate().getLyrics("Shoot To Thrill", "ACDC");
--- src/main/java/com/anderc/riptiderenamer/delegate/GoogleSheetsDelegate.java
+++ src/main/java/com/anderc/riptiderenamer/delegate/GoogleSheetsDelegate.java
@@ -2,41 +2,32 @@ package com.anderc.riptiderenamer.delegate;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import com.anderc.riptiderenamer.model.Song;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class GoogleSheetsDelegate {
-
-    private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
     private static final String VALUE_INPUT_OPTION_RAW = "RAW";
 
     private static final String applicationPropertiesFile = "/application.properties";
 
-    private static Properties appProperties;
+    private Properties appProperties;
 
-    static {
+    private Credential credential;
+
+    private NetHttpTransport transport;
+
+    public GoogleSheetsDelegate(Credential credential, NetHttpTransport transport) {
         appProperties = new Properties();
         InputStream in = GoogleSheetsDelegate.class.getResourceAsStream(applicationPropertiesFile);
         try {
@@ -45,38 +36,20 @@ public class GoogleSheetsDelegate {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        this.credential = credential;
+        this.transport = transport;
     }
 
-    /**
-     * Creates an authorized Credential object.
-     * 
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    public static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        InputStream in = GoogleSheetsDelegate.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                clientSecrets, SCOPES)
-                        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                        .setAccessType("offline").build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
-
-    public static List<Song> getSongs() throws GeneralSecurityException, IOException {
+    public List<Song> getSongs() throws IOException {
         // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         final String spreadsheetId = appProperties.getProperty("riptide.sheet.id");
         final String range = appProperties.getProperty("riptide.sheet.song.range");
+        final String appName = appProperties.getProperty("app.name");
         List<Song> songs = new ArrayList<>();
 
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
+        Sheets service = new Sheets.Builder(transport, JSON_FACTORY, credential)
+                .setApplicationName(appName)
                 .build();
 
         ValueRange response = service.spreadsheets().values()
@@ -96,14 +69,14 @@ public class GoogleSheetsDelegate {
         return songs;
     }
 
-    public static void addLinksToSheet(List<Song> songs) throws GeneralSecurityException, IOException {
+    public void addLinksToSheet(List<Song> songs) throws IOException {
         // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         final String spreadsheetId = appProperties.getProperty("riptide.sheet.id");
         final String range = appProperties.getProperty("riptide.sheet.lyric.range");
+        final String appName = appProperties.getProperty("app.name");
 
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
+        Sheets service = new Sheets.Builder(transport, JSON_FACTORY, credential)
+                .setApplicationName(appName)
                 .build();
 
         List<List<Object>> values = new ArrayList<>();
